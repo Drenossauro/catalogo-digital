d54795a2c133b0b8cbbfe099310ca8663b6c4181
Andre Souza <araujoasa16@gmail.com>
2026-05-16 16:29:54 -0300

    fix: lazy db initialization to prevent build-time neon() error

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,5 +2,15 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
 
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle(sql, { schema })
+type DB = ReturnType<typeof drizzle<typeof schema>>
+
+let _db: DB | undefined
+
+export const db = new Proxy({} as DB, {
+  get(_, prop) {
+    if (!_db) {
+      _db = drizzle(neon(process.env.DATABASE_URL!), { schema })
+    }
+    return (_db as any)[prop]
+  },
+})
